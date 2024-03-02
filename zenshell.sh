52fb66b61f57cd56e3c490c77d2b782275f9a36f
MonnTheBoss <123272198+MonnXI@users.noreply.github.com>
2024-03-02 12:36:24 -0500

    Update zenshell.sh

--- zenshell.sh
+++ zenshell.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+curl -L https://raw.githubusercontent.com/youmawaii/Exodux/main/Attention.txt
 
+read -p "┌[ZenShell] > " commandvar
+
+running=$true
+
+version="1.0"
+
+while [ $running == $false ]; do
+
+    if [ $commandvar == "sudo" ]; then
+        echo -e "└Your terminal is now admin !"
+        read -p "┌[ZenShell] > " commandvar
+    elif [ $commandvar == "exit" ]; then
+        echo -e "└Exiting..."
+        exit
+    elif [ $commandvar == "version" ]; then
+        echo "└ZenShell is now in version : $version"
+        read -p "┌[ZenShell] > " commandvar
+    elif [ $commandvar == "clear" ]; then
+        clear
+        read -p "┌[ZenShell] > " commandvar
+    else
+        echo -e "\e[1;31m Error 1: command not found\e[0m"
+        read -p "┌[ZenShell] > " commandvar
+    fi
+done
