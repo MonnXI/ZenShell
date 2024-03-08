c24e94936b77f1b32c745e8c290a0bcea0db39cf
MonnTheBoss <123272198+MonnXI@users.noreply.github.com>
2024-03-08 11:18:20 -0500

    Update zenshell.sh

--- zenshell.sh
+++ zenshell.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
-curl -L https://raw.githubusercontent.com/Lichen13/Exodux/main/Attention.txt?token=GHSAT0AAAAAACO4OPVONJFTELCXPGWPZPMAZPDTJYA
+latestVersion=$(curl -s 'https://raw.githubusercontent.com/MonnXI/ZenShell/main/latestVersion.txt')
+
+running=true
+
+version="1.0.1"
+
+if [ "$latestVersion" != "$version" ]; then
+    echo "IMPORTANT --- Your system needs an update !"
+fi
 
 read -p "┌[ZenShell] > " commandvar arg1
 
-running=$true
+while [ "$running" == true ]; do
 
-version="1.0.0"
-
-while [ $running == $false ]; do
-
-    if [ $commandvar == "sudo" ]; then
+    if [ "$commandvar" == "sudo" ]; then
         echo -e "└Your terminal is now admin !"
         read -p "┌[ZenShell] > " commandvar
-    elif [ $commandvar == "exit" ]; then
-        echo -e "└Exiting..."
+    elif [ "$commandvar" == "exit" ]; then
+        echo -n "└Exiting."
+        sleep 0.5
+        echo -n "."
+        sleep 0.5
+        echo "."
+        sleep 0.5
         exit
-    elif [ $commandvar == "version" ]; then
+    elif [ "$commandvar" == "version" ]; then
         echo "└ZenShell is now in version : $version"
-        read -p "┌[ZenShell] > " commandvar
-    elif [ $commandvar == "clear" ]; then
+        read -p "┌[ZenShell] > " commandvar arg1
+    elif [ "$commandvar" == "clear" ]; then
         clear
-        read -p "┌[ZenShell] > " commandvar
-    elif [ $commandvar == "module" ]; then
-        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' "jq"|grep "install ok installed")
+        read -p "┌[ZenShell] > " commandvar arg1
+    elif [ "$commandvar" == "module" ]; then
         echo -e "\e[1;31m└Error 2: command is none\e[0m"
-        read -p "┌[ZenShell] > " commandvar
+        if [ "$arg1" == "install" ]; then
+            echo "true"
+        fi
+        read -p "┌[ZenShell] > " commandvar arg1
     else
         echo -e "\e[1;31m└Error 1: command not found\e[0m"
-        read -p "┌[ZenShell] > " commandvar
+        read -p "┌[ZenShell] > " commandvar arg1
     fi
 done
