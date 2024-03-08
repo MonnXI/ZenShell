307e8d0f88dfced28c751a3d180f7aeb7ea85f54
MonnTheBoss <123272198+MonnXI@users.noreply.github.com>
2024-03-07 21:24:45 -0500

    Update zenshell.sh

--- zenshell.sh
+++ zenshell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -L https://raw.githubusercontent.com/Lichen13/Exodux/main/Attention.txt?token=GHSAT0AAAAAACO4OPVONJFTELCXPGWPZPMAZPDTJYA
 
-read -p "┌[ZenShell] > " commandvar
+read -p "┌[ZenShell] > " commandvar arg1
 
 running=$true
 
@@ -20,6 +20,10 @@ while [ $running == $false ]; do
         read -p "┌[ZenShell] > " commandvar
     elif [ $commandvar == "clear" ]; then
         clear
+        read -p "┌[ZenShell] > " commandvar
+    elif [ $commandvar == "module" ]; then
+        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' "jq"|grep "install ok installed")
+        echo -e "\e[1;31m└Error 2: command is none\e[0m"
         read -p "┌[ZenShell] > " commandvar
     else
         echo -e "\e[1;31m└Error 1: command not found\e[0m"
