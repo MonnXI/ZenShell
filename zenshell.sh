2bdb1b5345235e19de3b352ebd00b215e580e41b
MonnTheBoss <123272198+MonnXI@users.noreply.github.com>
2024-03-08 19:44:44 -0500

    Update zenshell.sh

--- zenshell.sh
+++ zenshell.sh
@@ -9,7 +9,7 @@ if [ "$latestVersion" != "$version" ]; then
     echo "IMPORTANT --- Your system needs an update !"
 fi
 
-read -p "┌[ZenShell] > " commandvar arg1
+read -p "┌[ZenShell] > " commandvar arg1 arg2
 
 while [ "$running" == true ]; do
 
@@ -24,21 +24,24 @@ while [ "$running" == true ]; do
     elif [ "$commandvar" == "version" ]; then
         echo "│ZenShell is now in version : $version"
         echo "└The latest version is : $latestVersion"
-        read -p "┌[ZenShell] > " commandvar arg1
+        read -p "┌[ZenShell] > " commandvar arg1 arg2
     elif [ "$commandvar" == "clear" ]; then
         clear
-        read -p "┌[ZenShell] > " commandvar arg1
+        read -p "┌[ZenShell] > " commandvar arg1 arg2
     elif [ "$commandvar" == "module" ]; then
         echo -e "\e[1;31m└Error 2: command is none\e[0m"
         if [ "$arg1" == "install" ]; then
             echo "true"
+        elif [ "$arg1" == "update" ]; then
+            curl -L "$arg2"
         fi
-        read -p "┌[ZenShell] > " commandvar arg1
+        read -p "┌[ZenShell] > " commandvar arg1 arg2
     elif [ "$commandvar" == "help" ]; then
-        echo -e "│Help menu : \n│Command help [no arguments]: show this menu\n│Command exit [no arguments]: exit the terminal\n│Command version [no arguments]: show the actual version and the latest version of ZenShell\n│Command clear [no arguments]: clear the terminal\n└Command module [install/remove/update]: manage the modules of ZenShell"
-        read -p "┌[ZenShell] > " commandvar arg1
+        echo -e "│Help menu : \n│Command help [no arguments]: show this menu\n│Command exit [no arguments]: exit the terminal\n│Command version [no arguments]: show the actual version and the latest version of ZenShell\n│Command clear [no arguments]: clear the terminal\n└Command module [install/remove/update] [package_url]: manage the modules of ZenShell"
+        read -p "┌[ZenShell] > " commandvar arg1 arg2
+        #ENTER MODULES HERE
     else
         echo -e "\e[1;31m└Error 1: command not found\e[0m"
-        read -p "┌[ZenShell] > " commandvar arg1
+        read -p "┌[ZenShell] > " commandvar arg1 arg2
     fi
 done
