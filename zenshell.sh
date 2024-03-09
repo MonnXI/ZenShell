6d0297e83b8d8fc6e863d0ab591cd485ad82b391
MonnTheBoss <123272198+MonnXI@users.noreply.github.com>
2024-03-09 16:51:37 -0500

    Update zenshell.sh

--- zenshell.sh
+++ zenshell.sh
@@ -3,13 +3,16 @@ latestVersion=$(curl -s 'https://raw.githubusercontent.com/MonnXI/ZenShell/main/
 
 running=true
 
+declare -A moduleNames
+beta=false
 version="1.0.1"
 
-if [ "$latestVersion" != "$version" ]; then
-    echo "IMPORTANT --- Your system needs an update !"
+if [ "$beta" == false ]
+    if [ "$latestVersion" != "$version" ]; then
+        echo "IMPORTANT --- Your system needs an update !"
+    fi
 fi
-
-read -p "┌[ZenShell] > " commandvar arg1 arg2
+read -p "┌[ZenShell] > " commandvar arg1 arg2 arg3 arg4
 
 while [ "$running" == true ]; do
 
@@ -24,10 +27,10 @@ while [ "$running" == true ]; do
     elif [ "$commandvar" == "version" ]; then
         echo "│ZenShell is now in version : $version"
         echo "└The latest version is : $latestVersion"
-        read -p "┌[ZenShell] > " commandvar arg1 arg2
+        read -p "┌[ZenShell] > " commandvar arg1 arg2 arg3 arg4
     elif [ "$commandvar" == "clear" ]; then
         clear
-        read -p "┌[ZenShell] > " commandvar arg1 arg2
+        read -p "┌[ZenShell] > " commandvar arg1 arg2 arg3 arg4
     elif [ "$commandvar" == "module" ]; then
         if [ "$arg1" == "" ]; then
             echo -e "module command:\narg1: install / remove / update\narg2: module_url / module_name"
@@ -35,22 +38,27 @@ while [ "$running" == true ]; do
             if [ "$arg2" == "" ]; then
                 echo -e "\e[1;31m└Error 3: missing arguments\e[0m"
             elif [ "$arg2" != "" ]; then
-                module_url=$(curl -s "$arg2")
-                echo -e "$module_url"
+                module=$(curl -s "$arg2")
+                module_name=$(curl -s "$arg2" | grep "name=")
+                if [ -n "$module_name" ]; then
+                    echo "$module_name"
+                else
+                    echo -e "\e[1;31m└Error 4: this is not a module\e[0m"
+                fi
             fi
         elif [ "$arg1" == "update" ]; then
             curl -L "$arg2"
         fi
-        read -p "┌[ZenShell] > " commandvar arg1 arg2
+        read -p "┌[ZenShell] > " commandvar arg1 arg2 arg3 arg4
     elif [ "$commandvar" == "help" ]; then
         echo -e "│Help menu : \n│Command help [no arguments]: show this menu\n│Command exit [no arguments]: exit the terminal\n│Command version [no arguments]: show the actual version and the latest version of ZenShell\n│Command clear [no arguments]: clear the terminal\n└Command module [install/remove/update] [module_url(install)/module_name(remove/update)]: manage the modules of ZenShell"
-        read -p "┌[ZenShell] > " commandvar arg1 arg2
+        read -p "┌[ZenShell] > " commandvar arg1 arg2 arg3 arg4
         #ENTER MODULES HERE
     elif [ "$commandvar" == "" ]; then
-        read -p "┌[ZenShell] > " commandvar arg1 arg2
+        read -p "┌[ZenShell] > " commandvar arg1 arg2 arg3 arg4
     else
         echo -e "\e[1;31m└Error 1: command not found\e[0m"
-        read -p "┌[ZenShell] > " commandvar arg1 arg2
+        read -p "┌[ZenShell] > " commandvar arg1 arg2 arg3 arg4
     fi
 done
 
@@ -58,3 +66,4 @@ done
 # Error 1: command not found
 # Error 2: command is none
 # Error 3: Missing arguments
+# Error 4: this is not a module
