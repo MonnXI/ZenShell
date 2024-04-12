7adc2a9e35bfe787cf64d09ff8af8bc3485506ac
MonnTheBoss <123272198+MonnXI@users.noreply.github.com>
2024-04-12 15:33:14 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#######################################################################
+# Program: ZenShell
+# Copyright (c) 2024 MonnTheBoss, Lichen
+# 
+# Usage:
+#   To use this script, run:
+#   ./install.sh
+#
+# This script is governed by the terms of the GNU General Public License v3.0
+# The latest version of the license can be found at:
+# https://www.gnu.org/licenses/gpl-3.0.html
+#######################################################################
+
+echo -e "Checking packages..."
+
+if ! which jq > /dev/null; then
+    read -p "jq not found! Install? (Y/n) \c" answer
+    if "$answer" = "Y"; then
+        sudo apt-get install jq
+    elif [ "$answer" == "" ]; then
+        sudo apt-get install jq
+    fi
+else
+    echo -e "jq is present on the system!"
+fi
+if ! which curl > /dev/null; then
+    read -p "cURL not found! Install? (Y/n) \c" answer
+    if "$answer" = "Y"; then
+        sudo apt-get install curl
+    elif [ "$answer" == "" ]; then
+        sudo apt-get install curl
+    fi
+else
+    echo -e "cURL is present on the system!"
+fi
+if ! which nmcli > /dev/null; then
+    read -p "nmcli not found! Install? (Y/n) \c" answer
+    if "$answer" = "Y"; then
+        sudo apt-get install nmcli
+    elif [ "$answer" == "" ]; then
+        sudo apt-get install nmcli
+    fi
+else
+    echo -e "nmcli is present on the system!"
+fi
