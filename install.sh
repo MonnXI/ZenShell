176e61a22622f895a0926bb6eb93e9234aa6aa52
MonnTheBoss <jfortinsimard@gmail.com>
2024-08-05 13:01:58 -0400

    commit

--- install.sh
+++ install.sh
@@ -53,15 +53,16 @@ read -p "Which version do you want to install [beta/stable]" release
 if [ "$release" == "stable" ]; then
     curl -O https://raw.githubusercontent.com/MonnXI/ZenShell/stable/zenshell.sh
     make zenshell
-    mv zenshell /usr/local/bin
+    sudo mv zenshell /usr/local/bin
     rm zenshell.sh
     exit
 elif [ "$release" == "beta" ]; then
     curl -O https://raw.githubusercontent.com/MonnXI/ZenShell/beta/zenshell.sh
     make zenshell
-    mv zenshell /usr/local/bin
+    sudo mv zenshell /usr/local/bin
     rm zenshell.sh
     exit
 else
     echo "Please enter a correct answer."
     read -p "Which version do you want to install [beta/stable]" release
+fi
